540523f1a832e4b04448f39e23c21ad9571b5787
RuslanA <wolfgenrifox@gmail.com>
2019-10-27 03:39:04 +0500

    TEST COMMIT TeamCity 6 build apk/ test vcs triger

--- app/src/main/java/com/example/jenkins_test/MainActivity.kt
+++ app/src/main/java/com/example/jenkins_test/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.jenkins_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import androidx.annotation.ColorRes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        val blue = R.color.blue
+        val red = R.color.red
+        val white = R.color.white
+        val green = R.color.green
+        val black = R.color.black
+
+        val colorName = listOf(black, red, white, green, blue)
+
+        iv_android.setOnClickListener {
+            it.setBackgroundColor(colorName.random())
+        }
     }
 }
